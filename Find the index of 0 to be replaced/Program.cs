12da92cea39fb19a36a028d5bd9d6b5f480fce44
Farnoud Taghdissi Borhani <v-ftaghd@microsoft.com>
2021-12-21 06:52:12 -0800

    Add project files.

---
+++ Find the index of 0 to be replaced/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Find_the_index_of_0_to_be_replaced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 0, 0, 1, 0, 1, 1, 1, 0, 1, 1 };
+
+            Console.WriteLine(findIndexofZero(A).ToString());
+        }
+
+        public static int findIndexofZero(int[] arr)
+        {
+            int MaxIdx = -1;
+            int PrvZero = -1;
+            int Count = 0;
+            int MaxCount = 1;
+            int len = arr.Length;
+            if (len < 2)
+                return len;
+            
+            for(int i =0; i < len; i++)
+            {
+                if (arr[i] == 1)
+                    Count++;
+                else
+                {
+                    Count = i - PrvZero;
+                    PrvZero = i;
+                }
+
+                if (Count > MaxCount)
+                { 
+                    MaxCount = Count;
+                    MaxIdx = PrvZero;
+                }
+            }
+
+            return MaxIdx;
+        } 
+    }
+}
